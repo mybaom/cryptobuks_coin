44294889719811be3b0dfd67e407970145e3688e
denni1024 <1656239759@qq.comm>
2021-10-08 20:20:35 +0800

    '1'

--- public/m/javascripts/main.js
+++ public/m/javascripts/main.js
@@ -7,7 +7,7 @@ var _SERVER = _DOMAIN + "/mobile/"; //域名
 var _API = _DOMAIN + "/api/";
 var paths = window.location.pathname;
 var parmas = window.location.search;
-var socket_api = _DOMAIN + ':' + 2000;
+var socket_api = _DOMAIN.replace(/https/, 'http') + ':' + 2000;
 if (localStorage.getItem('skin')) {
     var skins = localStorage.getItem('skin');
     var arr = paths.split('/');
