0e31a0b1111cee8f77ef4920b15ac833f08f83f3
denni1024 <1656239759@qq.comm>
2021-10-08 21:46:55 +0800

    '1'

--- public/m/javascripts/main.js
+++ public/m/javascripts/main.js
@@ -7,7 +7,7 @@ var _SERVER = _DOMAIN + "/mobile/"; //域名
 var _API = _DOMAIN + "/api/";
 var paths = window.location.pathname;
 var parmas = window.location.search;
-var socket_api = '140.82.20.117:2000';
+var socket_api = 'https://ws.cbvccx.com';
 if (localStorage.getItem('skin')) {
     var skins = localStorage.getItem('skin');
     var arr = paths.split('/');
