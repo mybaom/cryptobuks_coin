5c5df3d6a0caee47653f6330ca1c50d766941712
denni1024 <1656239759@qq.comm>
2021-10-21 23:30:54 +0800

    'a'

--- app/Http/Controllers/Api/WalletController.php
+++ app/Http/Controllers/Api/WalletController.php
@@ -89,7 +89,7 @@ class WalletController extends Controller
                 'is_lever' => 1,
                 'is_match' => 1,
                 'is_micro' => 0,
-                'lock_change_balance' => 0,
+                'lock_change_balance' => '0.000000',
                 'pb_price' =>  "0.00000000",
                 'usdt_price' => $offerWallet->now_price
             ];
@@ -102,7 +102,7 @@ class WalletController extends Controller
             if($k == 3 && $cbvWallet)
             {
                 $newChaneWallet[] = $cbvWallet;
-                $num = $cbvWallet['change_balance'];
+                $num = $cbvWallet['change_balance'] + $cbvWallet['lock_change_balance'];
                 // $change_wallet['totle'] += $num * $v['cny_price'];
                 $change_wallet['usdt_totle'] += $num * $cbvWallet['usdt_price'];
             }
