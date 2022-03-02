6c591bab4ff93854b44ec16aa38ff291f43d8995
deni <1820746994@qq.com>
2022-03-02 17:27:42 +0800

    cors

--- public/index.php
+++ public/index.php
@@ -5,6 +5,12 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
+// 指定允许其他域名访问
+header('Access-Control-Allow-Origin: *');
+// 响应类型
+header('Access-Control-Allow-Methods: *');
+// 响应头设置
+header('Access-Control-Allow-Headers: x-requested-with, content-type');
 
 define('LARAVEL_START', microtime(true));
 /*
