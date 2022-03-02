5951dab1b1fbc5c703fb200f813fb70c9f865f02
deni <1820746994@qq.com>
2022-03-02 17:58:39 +0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,7 @@ class Cors
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('X-Requested-Withs', 'XMLHttpRequest');
         return $response;
     }
 }
