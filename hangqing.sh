9c8d4ac84977b46c8885ddb0bff9344bfc4ac3a3
deni <1820746994@qq.com>
2022-08-25 13:40:14 +0800

    update hangqing.sh

--- hangqing.sh
+++ hangqing.sh
@@ -1,8 +1,9 @@
-step=5
 i=0
-while i<=60
-   do
-      echo /bin/sh /var/www/html/hangqing_handle.sh
-      i+=$step
-      sleep()
-   done
+step=5
+while(( $i<60 ))
+do
+    i=`expr $i + $step`
+    echo "$i"
+    echo /bin/sh /var/www/html/hangqing_handle.sh
+    sleep 5
+done
