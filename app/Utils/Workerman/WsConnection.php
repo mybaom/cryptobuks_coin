f255e4394420a899713c4961e29c77e08d22745a
deni <1820746994@qq.com>
2022-08-29 17:49:27 +0800

    update WsConnect

--- app/Utils/Workerman/WsConnection.php
+++ app/Utils/Workerman/WsConnection.php
@@ -12,8 +12,8 @@ use App\Jobs\{CoinTradeHandel, EsearchMarket, LeverUpdate, LeverPushPrice, SendM
 
 class WsConnection
 {
-    protected $server_address = 'ws://api.huobi.pro:443/ws';
-//    protected $server_address = 'ws://api.huobipro.com/ws'; //ws国内开发调试
+//    protected $server_address = 'ws://api.huobi.pro:443/ws';
+    protected $server_address = 'ws://api.huobipro.com/ws'; //ws国内开发调试
     protected $server_ping_freq = 1; //服务器ping检测周期,单位秒
     protected $server_time_out = 5; //服务器响应超时
     protected $send_freq = 3; //写入和发送数据的周期，单位秒
